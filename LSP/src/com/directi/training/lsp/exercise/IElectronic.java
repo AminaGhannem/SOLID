353b91ca1dad30ff8201836141ec05e239f33b43
yasminekharrat123 <yasmine.kharrat@insat.ucar.tn>
2025-02-27 22:24:12 +0100

    refactor LSP

---
+++ LSP/src/com/directi/training/lsp/exercise/IElectronic.java
@@ -0,0 +1,7 @@
+package com.directi.training.lsp.exercise;
+
+public interface IElectronic {
+    void turnOn();
+
+    void turnOff();
+}
--- LSP/src/com/directi/training/lsp/exercise/NormalDuck.java
+++ LSP/src/com/directi/training/lsp/exercise/NormalDuck.java
@@ -1,14 +1,13 @@
 package com.directi.training.lsp.exercise;
 
-public class Duck
-{
-    public void quack()
-    {
+public class NormalDuck implements IDuck {
+    @Override
+    public void quack() {
         System.out.println("Quack...");
     }
 
-    public void swim()
-    {
+    @Override
+    public void swim() {
         System.out.println("Swim...");
     }
 }
